ff9c3fe770438191389073c8dabd03c62b40629a
Nemental <15136847+Nemental@users.noreply.github.com>
2023-11-20 08:10:53 +0000

    fix: csrf validation logic

--- src/index.php
+++ src/index.php
@@ -4,15 +4,22 @@ session_start();
 
 require __DIR__ . '/vendor/autoload.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 $response = ['success' => false, 'message' => ''];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require __DIR__ . '/includes/validateCsrfToken.php';
+    $skipCsrf = filter_var(getenv('CSRF_SKIP'), FILTER_VALIDATE_BOOLEAN);
+    $validateCsrfToken = false;
 
-    if (validateCsrfToken()) {
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
+    if (!$skipCsrf) {
+        require __DIR__ . '/includes/validateCsrfToken.php';
+        $validateCsrfToken = validateCsrfToken();
+    }
 
+    if ($validateCsrfToken || $skipCsrf) {
         require __DIR__ . '/includes/verifyCaptcha.php';
         require __DIR__ . '/includes/processForm.php';
 

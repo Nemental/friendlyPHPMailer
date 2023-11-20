0ee733e0957693721a01c5090cfab3eb3eb1035e
Nemental <15136847+Nemental@users.noreply.github.com>
2023-11-20 14:24:42 +0000

    fix: typo

--- src/includes/validateCsrf.php
+++ src/includes/validateCsrf.php
@@ -6,7 +6,6 @@ function validateCsrf() {
         hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])
     ) {
         return ['success' => true];
-        );
     } else {
         return ['success' => false, 'message' => 'Invalid CSRF token!'];
     }
